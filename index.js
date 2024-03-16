e2c4ee4e104ece64cff20364f5954a346cb43397
Sebastian Strahinja Banovic <strahinja.s.banovic@gmail.com>
2024-03-16 13:42:47 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Import libraries
 const puppeteer = require('puppeteer');
 const fs = require("fs");
 const locateChrome = require('locate-chrome');
 
+// Function to find and click on an element
 findAndClick = async (page, selector) => {
     await page.waitForSelector(selector);
     await page.click(selector);
@@ -9,6 +11,8 @@ findAndClick = async (page, selector) => {
     return;
 };
 
+// Function to set text in a text field
+// and return true if no text fields are found or false otherwise
 setText = async (page, selector, text) => {
     
     let noTextFields = true;
@@ -25,6 +29,8 @@ setText = async (page, selector, text) => {
     return noTextFields;
 };
 
+// Function to select options in a select box
+// and return true if no select boxes are found or false otherwise
 selectOptions = async (page, selector) => {
     let noSelectBoxes = true;
 
@@ -43,6 +49,7 @@ selectOptions = async (page, selector) => {
     return noSelectBoxes;
 };
 
+// Function to simulate checkout payment
 simulateCheckout = async (page) => {
     // Go to cart page
     await page.goto('https://www.etsy.com/cart?ref=hdr-cart', {waitUntil: 'networkidle2'}).then(async () => {
@@ -64,49 +71,61 @@ simulateCheckout = async (page) => {
     });
 };
 
+// Main function
 (async () => {
-    // Launch the browser TODO straja
+
+    // Find the path of the Chrome executable
     const executablePath = await new Promise(resolve => locateChrome(arg => resolve(arg)));
+    // Launch browser and set options for preview and debugging
     const browser = await puppeteer.launch({ headless: false, executablePath, dumpio: true });
   
     // Create a page
     const page = await browser.newPage();
 
-    //Set viewpoint of browser page
+    //Set dimensions of viewpoint for browser page
     await page.setViewport({ width: 600, height: 1500 })
   
-    // Go to site
+    // Go to site and wait for it to load until there are no more than 2 network connections for at least 500ms
     await page.goto('https://www.etsy.com', {waitUntil: 'networkidle2'});
 
+    // Set the maximum navigation time to 500000ms which is 500 seconds
     page.setDefaultNavigationTimeout(500000);
 
+    // Take a screenshot of main page
     await page.screenshot({
         path: 'etsy.png'
     });
   
-    // Query for an element handle.
+    // Click on the first link of category elements and wait for the page to load
     await findAndClick(page, 'a.wt-card__action-link');
 
+    // Take a screenshot of the first step
     await page.screenshot({
         path: 'etsy-1-step.png'
     });
 
+    // Click on the second link of category elements and wait for the page to load
     await findAndClick(page, 'a.parent-hover-underline');
 
+    // Take a screenshot of the second step
     await page.screenshot({
         path: 'etsy-2-step.png'
     });
 
+    // Click on the third link of category elements and wait for the page to load
     await findAndClick(page, 'a.parent-hover-underline');
     
+    // Take a screenshot of the third step
     await page.screenshot({
         path: 'etsy-3-step.png'
     });
 
+    // Find all the rings on the page and set their names, prices and urls
     const names = await page.evaluate(() => Array.from(document.querySelectorAll('h2.wt-text-caption'), element => element.textContent));
     const prices = await page.evaluate(() => Array.from(document.querySelectorAll('span.currency-value'), element => element.textContent));
     const urls = await page.evaluate(() => Array.from(document.querySelectorAll('a[href].listing-link'), element => element.getAttribute('href')));
 
+    // Create a JSON object with the rings
     let rings = [];
     for(let i = 0; i < 10; i++) {
         let ring =
@@ -118,6 +137,7 @@ simulateCheckout = async (page) => {
         rings.push(ring);
     }
 
+    // Write the JSON object to a file
     fs.writeFile(
         "./db.json",
         JSON.stringify(rings),
@@ -125,19 +145,26 @@ simulateCheckout = async (page) => {
             // Checking for errors 
             if (err) throw err;
     
-            // Success 
+            // After Success go trough the rings and add them to the cart if possible
             for(let i = 0; i < 10; i++) {
 
+                // Go to the ring page and wait for it to load until there are no more than 2 network connections
                 await page.goto(rings[i].url, {waitUntil: 'networkidle2'}).then(async () => {
+                    // Set main part of selector for select boxes
                     let selector = 'select#variation-selector-';
                 
+                    // Select options
                     await selectOptions(page, selector).then(async (noSelectBoxes) => {
+                        // Set selector for text fields
                         selector = 'textarea#listing-page-personalization-textarea';
 
+                        // and set text in text fields
                         await setText(page, selector, "T").then(async (noTextFields) => {
+                            // If there are no select boxes and no text fields, click on the add to cart button
                             if(noSelectBoxes && noTextFields)
                                 await findAndClick(page, 'div[data-selector=add-to-cart-button] > button');
 
+                            // Take a screenshot of the ring with selected options and added text
                             await page.screenshot({
                                 path: 'ring'+i+'.png'
                             });
@@ -147,8 +174,10 @@ simulateCheckout = async (page) => {
 
             }
 
+            // Simulate checkout payment
             await simulateCheckout(page);
 
+            // Close the browser
             await browser.close();
         });
 })();
